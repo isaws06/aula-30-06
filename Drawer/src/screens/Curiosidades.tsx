588f0d91fa1ae9b744b01c7c93ea96a81f29849b
Isadora Wenzel da Silveira <99267300+isaws06@users.noreply.github.com>
2023-07-03 10:51:31 -0100

    Update Curiosidades.tsx

--- Drawer/src/screens/Curiosidades.tsx
+++ Drawer/src/screens/Curiosidades.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Button, Text, TouchableOpacity} from 'react-native';
+import { View, StyleSheet, Button, Text, TouchableOpacity, Image} from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
 import { useNavigation } from '@react-navigation/native';
@@ -11,19 +11,39 @@ type ParamsProps = {
 export function Curiosidades() {
     
     const route = useRoute();
-    const { name } = route.params as ParamsProps;
+   
     
     const navigation = useNavigation();
   
     function openScreen(){
-        navigation.navigate('Home')
+        navigation.navigate('Fotos')
     }
   
     return (
     <View style={styles.container}>
-        <Text style={styles.text}>
-            { name }
-        </Text>
+        <Image style={styles.image} source={require('../../assets/a1.jpg')}></Image>
+    <Text style={styles.text}>Site do Aladim</Text>
+<br />
+    <Text style={styles.text1}>Curiosidades</Text>
+    <br />
+    <Text style={styles.titulo1}>
+      <span style={styles.bold}>Idade:</span> 9 anos
+      <br />
+      <span style={styles.bold}>Peso:</span> 5kg
+      <br />
+      <span style={styles.bold}>Comida Favorita:</span> Banana
+      <br />
+      <span style={styles.bold}>Brinquedo Favorito:</span> Um macaco de pelúcia
+      <br />
+      <span style={styles.bold}>Outras Curiosidades:</span> Consegue sentir o cheiro de banana de longe, <br />
+      parece um hidrante de tanto que mija, 
+      gosta de dormir de conchinha, <br />
+      ODEIA que pessoas estranhas entrem no banheiro da casa, <br />
+      não tem noção do seu proprio tamanho e tenta avançar em cachorros que são <br />o triplo 
+      do  seu tamanho, ele é meio suicida tanto que já pulou de um beliche e 
+      ele é tarado por braços.
+   </Text>
+<br />
 
         <TouchableOpacity style={styles.Button} onPress={openScreen}>
             <Text style={styles.buttonText}>Ver fotos</Text>
@@ -45,15 +65,35 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center"
     },
-    text:{
-        color: "#fff",
-    },
+   
+      text:{
+        fontSize: 32,
+        fontWeight: 'bold',
+        color: '#6B4F4F',
+      },
 
+      text1:{
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#6B4F4F',
+        textAlign: 'left',
+      },
+
+      bold:{
+        fontWeight: 'bold',
+      },
+
+      titulo1:{
+        fontSize: 15,
+        textAlign: 'center',
+        color: '#6B4F4F',
+      },
+    
     Button: {
         backgroundColor: '#4F709C',
-        padding: 8,
+        padding: 10,
         borderRadius: 5,
-        width: '30%',
+        width: '10%',
         alignItems: 'center',
         marginTop: 15,
         shadowOffset: {
@@ -69,5 +109,12 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 15,
         fontFamily: 'bold',
+      },
+
+      image:{
+        width: 250,
+        height: 250,
+        borderRadius: 50,
+        margin: 10,  
       },
 })
